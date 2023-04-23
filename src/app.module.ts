79b7920d6488e46587083e13d80ada0e63e6706c
Aristide Fumo <aristide.ff@gmail.com>
2023-04-23 21:49:40 +0200

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { FriendRequestModule } from './friend-request/friend-request.module';
 import { FriendRequest } from './friend-request/friend-request.entity';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.HOST,
       port: 5432,
       entities: [User, FriendRequest],
-      username: 'postgres',
-      password: 'postgres',
-      database: 'game',
-      synchronize: true,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: process.env.SYNCHRONIZE === 'true',
     }),
     AuthModule,
     UsersModule,
