b5d9a9a5913e3ec0609f633379a527ab775372d6
trofinn <trofin.nicu98@gmail.com>
2023-04-23 14:41:05 +0200

    add entity

---
+++ src/room-party/room-party.entity.ts
@@ -0,0 +1,35 @@
+import { User } from 'src/users/user.entity';
+import { Column, Entity, JoinTable, ManyToOne, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class RoomParty {
+    @PrimaryGeneratedColumn()
+    id : number;
+
+    @Column()
+    name : string;
+
+    @Column()
+    game : string;
+
+    @ManyToMany(() => User, user => user.currentRooms, {nullable: true})
+    @JoinTable()
+    currentPlayers : User[];
+
+    @ManyToOne(() => User, user => user.createdRooms)
+    creator : User;
+
+    @Column()
+    gameStarted : boolean;
+
+    @Column()
+    terminated : boolean;
+
+    @Column()
+    saved : boolean;
+
+    @ManyToMany(() => User, {nullable: true})
+    @JoinTable()
+    winners : User[];
+
+}
