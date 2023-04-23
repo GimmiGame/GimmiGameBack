6a13c0167b796120663229e5b4a98df4a3777334
Aristide Fumo <aristide.ff@gmail.com>
2023-04-23 22:20:53 +0200

    add .env for ci /cd

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  dotenv.config();
+  await app.listen(process.env.PORTAPP as string);
 }
 bootstrap();
