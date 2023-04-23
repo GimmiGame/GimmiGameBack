239e7908ec103d2b1ee4ddc0d35d566364954fd5
trofinn <trofin.nicu98@gmail.com>
2023-04-23 14:44:59 +0200

    add module providers

--- src/room-party/room-party.module.ts
+++ src/room-party/room-party.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { RoomPartyService } from './room-party.service';
 import { RoomPartyController } from './room-party.controller';
+import { RoomPartyService } from './room-party.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomParty } from './room-party.entity';
+import { User } from 'src/users/user.entity';
+import { RoomPartyInfrastructure } from './room-party.infrastructure';
+import { UsersInfrastructure } from 'src/users/user.infrastructure';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [RoomPartyService],
-  controllers: [RoomPartyController]
+
+  imports: [
+    TypeOrmModule.forFeature([RoomParty]),
+    TypeOrmModule.forFeature([User]),
+    UsersModule
+    ],
+  controllers: [RoomPartyController],
+  providers: [RoomPartyService, RoomPartyInfrastructure, UsersInfrastructure],
+  exports: [RoomPartyService, RoomPartyInfrastructure]
 })
 export class RoomPartyModule {}
